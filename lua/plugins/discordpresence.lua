51bbbed264c6ec2989073854309dd6c4c8f70cff
TeenCoder159 <no1umang@gmail.com>
2025-06-13 13:22:25 +0800

    Modify discord presence

--- lua/plugins/discordpresence.lua
+++ lua/plugins/discordpresence.lua
@@ -11,12 +11,14 @@ return {
           local path = vim.fn.expand("%:p")
           if path:match("" .. home .. "/.config/nvim/") then
             return "Modifying nvim configs like a pro"
-          elseif path:match("" .. home .. "/Documents/Coding_stuff/rust/project/oneAI") then
+          elseif path:match("" .. home .. "/Documents/onellm/") then
               return "SaaS-ing out OneLLM"
           elseif path:match("" .. home .. "/Documents/Coding_stuff/rust/project/fastnd") then
             return "Rusting out a rust based numpy"
           elseif path:match("" .. home .. "/Documents/Coding_stuff/rust/project/") then
             return "Fighting the borrow checker"
+          elseif path:match("" .. home .. "/Documents/Coding_stuff/comps") then
+            return "Competing with competiton."
           elseif path:match("" .. home .. "/Documents/Coding_stuff/python/projects/") then
                 return "WHY IS IT SO GOD DAMN SLOWWWWW"
 
