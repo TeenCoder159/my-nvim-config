89867538e8d197a009ac02012779f57ce009a41c
unsecretised <no1umang@gmail.com>
2026-05-13 06:37:01 +0800

    Revert "discord and lua lsp"
    This reverts commit ca959bf9a084d47d9ff8fb0e4d5a86a84f828669.

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -4,45 +4,6 @@ return {
   opts = {
 	  display = { 
 		  theme = 'catppuccin',
-	  },
-      assets = {
-  ['.rs'] = {
-    tooltip = 'Rust.. Stop taking ownership',
-    text = function(opts)
-      return 'Fighting the borrow checker in ' .. opts.filename .. ' 🦀'
-    end,
-  },
-  ['.ts'] = {
-    tooltip = 'An attempt at a better JavaScript.. (A Failed one)',
-    text = function(opts)
-      return 'Barely surviving in' .. opts.filename
-    end,
-  },
-  ['.js'] = {
-    tooltip = 'An worse TypeScript',
-    text = function(opts)
-      return 'Barely surviving in' .. opts.filename
-    end,
-  },
-  ['.tsx'] = {
-    tooltip = 'Better than JSX',
-    text = function(opts)
-      return 'React-ing to hydrochloric acid in ' .. opts.filename
-    end,
-  },
-  ['.jsx'] = {
-    tooltip = 'Worse than TSX',
-    text = function(opts)
-      return 'React-ing to hydrochloric acid in ' .. opts.filename
-    end,
-  },
-  ['.py'] = {
-    tooltip = 'Python.. it\'s pure torture',
-    text = function(opts)
-      return 'Dying in ' .. opts.filename
-    end,
-  },
-},
-
+	  }
   }
 }
